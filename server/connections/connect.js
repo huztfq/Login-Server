5a08f8006930c00373f5059401e159b0f7c54c42
huzfq <huxaifa.tfq@outlook.com>
2023-12-08 22:31:17 +0500

    v 1.3

--- server/connections/connect.js
+++ server/connections/connect.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const connectToOnlineDB = async () => {
   try {
     const onlineURI = process.env.MONGODB_CONNECT_URI;
-    await mongoose.connect(onlineURI);
+    if (!onlineURI) {
+      throw new Error('MONGODB_CONNECT_URI is not defined in the environment variables.');
+    }
+
+    await mongoose.connect(onlineURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log('Connected to Cloud MongoDB Server');
   } catch (error) {
     console.error('ERROR:', error.message);
@@ -13,7 +21,11 @@ const connectToOnlineDB = async () => {
     const localURI = 'mongodb://127.0.0.1:27017/avrox';
 
     try {
-      await mongoose.connect(localURI);
+      await mongoose.connect(localURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
       console.log('Connected to Local MongoDB Server');
     } catch (localError) {
       console.error('ERROR:', localError.message);
