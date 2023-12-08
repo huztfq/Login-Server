610fa6eb7efcdc7821a5c10c127f139343da4f93
huzfq <huxaifa.tfq@outlook.com>
2023-12-08 21:24:49 +0500

    v 1.3

--- server/api/index.js
+++ server/api/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the portal");
 });
 
-app.use("/", userRoute);
+app.use("/user", userRoute);
 
 // Set up other middleware and configurations as needed
 
