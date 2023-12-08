39d1d9f13e9773c915217872dcc496096ad5c1f3
huzfq <huxaifa.tfq@outlook.com>
2023-12-08 19:29:55 +0500

    Update connect.js

--- server/connections/connect.js
+++ server/connections/connect.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose')
+require('dotenv').config();
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Database Server Connected")
-    } catch (error) {
-        console.log("ERROR: " + error.message )
-    }
+const mongoose = require('mongoose');
+
+// Check if MONGO_CONNECT_URI is defined in .env
+if (!process.env.MONGODB_CONNECT_URI) {
+  console.error('ERROR: Missing environment variable MONGODB_CONNECT_URI');
+  process.exit(1);
 }
 
-module.exports = connectDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+    console.log('Database Server Connected');
+  } catch (error) {
+    console.error('ERROR:', error.message);
+  }
+};
+
+module.exports = connectDB;
