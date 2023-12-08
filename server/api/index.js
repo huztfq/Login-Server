90cf2dd60f9951b46a9415c2508ea93027818e6a
huzfq <huxaifa.tfq@outlook.com>
2023-12-08 19:34:13 +0500

    v 1.3

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const connectDB  = require("./connections/connect");
-const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const connectDB  = require("../connections/connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require("../middlewares/auth");
 require('dotenv').config();
 
-const userRoute = require("./routes/user");
+const userRoute = require("../routes/user");
 
 const app = express();
 const PORT = 8001;
