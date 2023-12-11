222d804407ef279036d6592a4cdcbd48272b3cfe
huzfq <huxaifa.tfq@outlook.com>
2023-12-11 20:27:28 +0500

    v 1.3
    @bugless28
    Made Changes in the connection.js to add the cluster ID in the .env
    Added env.prod for the template
    Have placed the index file outside the /api

--- server/connections/connect.js
+++ server/connections/connect.js
@@ -1,22 +1,18 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectToOnlineDB = async () => {
   const maxAttempts = 5; // Set the maximum number of reconnection attempts
   let attempts = 0;
-
   while (attempts < maxAttempts) {
     try {
-      const onlineURI = 'mongodb+srv://User1:avrox123@avrox.8hegjnp.mongodb.net/?retryWrites=true&w=majority';
+      const onlineURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERID}/?retryWrites=true&w=majority` //third env cluster id
       if (!onlineURI) {
         throw new Error('MONGODB_CONNECT_URI is not defined in the environment variables.');
       }
-
       await mongoose.connect(onlineURI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true, 
       });
-
       console.log('Connected to Cloud MongoDB Server');
       return; // Exit the function if connection is successful
     } catch (error) {
@@ -26,9 +22,7 @@ const connectToOnlineDB = async () => {
       await new Promise(resolve => setTimeout(resolve, 5000)); // Delay for 5 seconds before reattempt
     }
   }
-
   console.error('Failed to connect to the online MongoDB server after multiple attempts.');
   process.exit(1);
 };
-
 module.exports = connectToOnlineDB;
