974cb1871cc9c286195d5c327df094873837788e
huzfq <huxaifa.tfq@outlook.com>
2023-12-14 18:57:34 +0500

    Changes after Review
    Made changes to the code as required

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,14 +3,12 @@ import { ThemeService } from './core/services/theme.service';
 import { RouterOutlet } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule,NgClass, RouterOutlet],
+  imports: [ReactiveFormsModule, NgClass, RouterOutlet],
 })
 export class AppComponent {
   title = 'Avrox PTO';
