e0753b83ad800c6e238e9532b42595b09aba4706
Huzaifa Tofeeq <huxaifa.tfq@outlook.com>
2023-12-22 01:08:35 +0500

    Update leave.js

--- server/controllers/leave.js
+++ server/controllers/leave.js
@@ -91,7 +91,13 @@ const getUserLeaveRequests = async (req, res) => {
 
     return res.status(200).json({ user: user.name, leaveRequests: formattedLeaveRequests });
   } catch (error) {
-    console.error(error);
+    console.error('Error in getUserLeaveRequests:', error);
+
+    // Log specific information about the error
+    if (error.name === 'CastError') {
+      return res.status(400).json({ message: 'Invalid userId format' });
+    }
+
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
