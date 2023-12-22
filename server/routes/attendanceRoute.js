4ae7319b5869ab135688ffe569fb54a7b2efb32a
Huzaifa Tofeeq <huxaifa.tfq@outlook.com>
2023-12-23 04:37:23 +0500

    Update attendanceRoute.js

--- server/routes/attendanceRoute.js
+++ server/routes/attendanceRoute.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const {createAttendance, getDayAttendance, fetchAllUsersAttendance, createLeave, getAttendance} = require("../controllers/hours");
+const {createAttendance, getDayAttendance, fetchAllUsersAttendance,} = require("../controllers/hours");
 const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
 
 // ROUTES FOR DATA ENTRY
-router.post("/createAtt/:userId", authMiddleware.authenticateToken, createAttendance);
-router.post("/createLeave/:id", authMiddleware.authenticateToken, createLeave);
+router.post("/createAtt/:userId", createAttendance);
 
 // ROUTES FOR FETCHING SINGLE DATA
-router.get("/getAtt/:id", authMiddleware.authenticateToken, getDayAttendance);
+router.get("/getAtt/:id", getDayAttendance);
 
 // ROUTES FOR FETCHING MULTIPLE ENTRIES
-router.get("/allAtt", authMiddleware.authenticateToken, fetchAllUsersAttendance);
-
-// NEW ROUTES FOR FETCHING
-// SINGLE ENTRY
-router.get("/getAttendance/:id", authMiddleware.authenticateToken, getAttendance); 
+router.get("/allAtt", fetchAllUsersAttendance);
 
 module.exports = router;
