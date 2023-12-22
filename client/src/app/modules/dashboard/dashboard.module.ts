7d9fa35367393a2ebc641a35fd8f9f07bd9c46a2
huzfq <huxaifa.tfq@outlook.com>
2023-12-22 16:31:16 +0500

    Resolving Conflict

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -4,7 +4,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddAttendaceComponent } from './pages/add-attendace/add-attendace.component';
 import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 import { ViewPtoComponent } from './pages/view-pto/view-pto.component';
@@ -18,14 +18,33 @@ import { LoadingComponent } from '../layout/components/loading/loading.component
 import { AddRequestComponent } from './pages/make-request-and-request-details/add-request.component';
 import { GetRequestComponent } from './pages/get-Request-and-approve/get-request.component';
 
-
 @NgModule({
-  imports: [DashboardRoutingModule, CommonModule, FormsModule, AuthModule, HttpClientModule, LoadingComponent],
-  declarations: [HomeComponent, AddAttendaceComponent, AddEmployeeComponent, ViewPtoComponent, AddRequestComponent, GetRequestComponent],
-  providers: [AuthService, DashboardService, AuthGuard,  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true,
-  },]
+  imports: [
+    DashboardRoutingModule,
+    CommonModule,
+    FormsModule,
+    AuthModule,
+    HttpClientModule,
+    LoadingComponent,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    HomeComponent,
+    AddAttendaceComponent,
+    AddEmployeeComponent,
+    ViewPtoComponent,
+    AddRequestComponent,
+    GetRequestComponent,
+  ],
+  providers: [
+    AuthService,
+    DashboardService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class DashboardModule {}
