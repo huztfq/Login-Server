8c96d5528eeb403e68b273d183dc44c81d849330
Huzaifa Tofeeq <huxaifa.tfq@outlook.com>
2023-12-29 10:23:47 -0500

    added multiple days feature

--- client/src/app/modules/dashboard/pages/make-request-and-request-details/add-request.component.ts
+++ client/src/app/modules/dashboard/pages/make-request-and-request-details/add-request.component.ts
@@ -4,42 +4,61 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DashboardService } from '../../services/dashboard.service';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { ISubmitRequest } from '../../models/user.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-request',
   templateUrl: './add-request.component.html',
-  styleUrls: ['./add-request.component.scss']
+  styleUrls: ['./add-request.component.scss'],
 })
 export class AddRequestComponent implements OnInit {
   private userId: string = '';
   leaveDetails: any;
-  selectedDate: string = new Date().toISOString().split('T')[0];
-  leaveRequest: { date: string, leaveType: 'casual' | 'sick' } = { date: '', leaveType: 'casual' };
   showLeaveRequestForm: boolean = false;
+  dateForm!: FormGroup;
+  multipleDays: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private dashboardService: DashboardService,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder,
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.userId = params.get('userId') || '';
       this.getLeaveDetails();
     });
+
+    this.dateForm = this.fb.group({
+      startDate: ['', Validators.required],
+      endDate: [''],
+      selection: ['', Validators.required],
+      reason: ['', Validators.required],
+    });
+  }
+
+  validateDate() {
+    const startDate = this.dateForm.get('startDate')?.value;
+    const endDate = this.dateForm.get('endDate')?.value;
+    if (startDate && endDate && startDate >= endDate) {
+      this.dateForm.get('endDate')?.setErrors({ invalidDate: true });
+    } else {
+      this.dateForm.get('endDate')?.setErrors(null);
+    }
   }
 
   getLeaveDetails() {
     const userId = this.authService.getUserData()?.userId;
-    this.dashboardService.getLeaveDetailsById(userId ?? "").subscribe(
+    this.dashboardService.getLeaveDetailsById(userId ?? '').subscribe(
       (response: any) => {
         this.leaveDetails = response;
       },
       (error) => {
         console.error('Error fetching leave details', error);
-      }
+      },
     );
   }
 
@@ -48,28 +67,51 @@ export class AddRequestComponent implements OnInit {
   }
 
   submitLeaveRequest() {
-    // Ensure leaveRequest.date is set to the selectedDate
-    this.leaveRequest.date = this.selectedDate;
-  
-    const data: ISubmitRequest = {
-      startDate: this.leaveRequest.date,
-      leaveType: this.leaveRequest.leaveType,
-    };
-    
-    console.log(data);
-  
-    this.dashboardService.makeLeaveRequest(this.authService.getUserData()?.userId ?? '', data).subscribe(
-      (response: any) => {
-        this.showLeaveRequestForm = false;
-        this.getLeaveDetails();
-      },
-      (error) => {
-        console.error('Error making leave request', error);
-      }
-    );
+    if(this.multipleDays){
+      this.validateDate();
+    }
+
+    if (!this.dateForm.valid) {
+      return;
+    }
+
+    console.log(this.dateForm.value, this.multipleDays);
+    return;
+
+    // const data: ISubmitRequest = {
+    //   startDate: this.leaveRequest.to,
+    //   leaveType: this.leaveRequest.leaveType,
+    // };
+
+    // console.log(this.leaveRequest);
+
+    return;
+    // this.dashboardService.makeLeaveRequest(this.authService.getUserData()?.userId ?? '', data).subscribe(
+    //   (response: any) => {
+    //     this.showLeaveRequestForm = false;
+    //     this.getLeaveDetails();
+    //   },
+    //   (error) => {
+    //     console.error('Error making leave request', error);
+    //   },
+    // );
   }
-  
+
   toggleLeaveRequestForm() {
     this.showLeaveRequestForm = !this.showLeaveRequestForm;
+  }
+
+  toggleMultipleDay() {
+    this.multipleDays = !this.multipleDays;
+
+    const endDateControl = this.dateForm.get('endDate');
+
+    if (this.multipleDays) {
+      endDateControl?.setValidators([Validators.required]);
+    } else {
+      endDateControl?.clearValidators();
+    }
+
+    endDateControl?.updateValueAndValidity();
   }
 }
