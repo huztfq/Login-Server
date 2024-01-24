ae20a760c7be9d782e699ac6ee83561bfa97d2e5
huzfq <huxaifa.tfq@outlook.com>
2024-01-25 02:40:22 +0500

    Added Reset Password
    Added 2 reset password controllers

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { handleUserSignup, handleUserLogin, handleUserResetPassword, handleUserForgotPassword, handleUserLogout, handleInfo, handleDeleteEmployees, handleFetchEmployeeDetails, handleUpdateEmployeeDetails, handleDeleteEmployee} = require("../controllers/user");
+const { handleUserSignup, handleUserLogin, handleUserResetPassword, handleInfo, handleDeleteEmployees, handleFetchEmployeeDetails, handleUpdateEmployeeDetails, handleDeleteEmployee, handleForgotPassword, handleVerifyOTP,} = require("../controllers/user");
 const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
@@ -8,13 +8,13 @@ const router = express.Router();
 router.post("/signup", authMiddleware.authenticateToken, handleUserSignup);
 router.post("/login", handleUserLogin);
 router.post("/reset", authMiddleware.authenticateToken, handleUserResetPassword);
-router.post("/forgot", authMiddleware.authenticateToken, handleUserForgotPassword);
-router.post("/logout", authMiddleware.authenticateToken, handleUserLogout);
 router.get("/info", handleInfo);
 router.post("/deleteEmployees/:userId",authMiddleware.authenticateToken, handleDeleteEmployees);
 router.get("/fetchEmployeeDetails/:userId", authMiddleware.authenticateToken, handleFetchEmployeeDetails);
 router.post("/updateEmployeeDetails/:userId", authMiddleware.authenticateToken, handleUpdateEmployeeDetails);
 router.post("/deleteEmployee/:userId", authMiddleware.authenticateToken, handleDeleteEmployee);
+router.post("/forgotPassword", handleForgotPassword);
+router.post("/verifyOTP", handleVerifyOTP);
 
 
 module.exports = router;
